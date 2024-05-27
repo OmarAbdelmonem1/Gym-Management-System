f746af6c81df0265ecb12fb17cf048e4c4832a02
Magdy372 <k.m334422@gmail.com>
2024-05-27 15:07:08 +0300

    Dashboard MVC

--- WindowsFormsApp1/views/DashBoardForms/DashBoardForm.cs
+++ WindowsFormsApp1/views/DashBoardForms/DashBoardForm.cs
@@ -22,17 +22,22 @@ namespace WindowsFormsApp1
         private Timer autoCheckOutTimer;
         private CheckInController checkInController;
 
+        private DashboardController dashboardController;
+
         public DashBoardForm()
         {
             InitializeComponent();
+            checkInController = new CheckInController();
+            dashboardController = new DashboardController();
+
             dbConnection = DBConnection.GetInstance();
             connection = dbConnection.GetConnection();
             LoadSubscriptionCounts();
 
-            checkInController = new CheckInController();
+            
             InitializeTimer();
 
-            var counts = FetchSubscriberCounts();
+            var counts = dashboardController.FetchSubscriberCounts();
             int platinumCount = counts.PlatinumCount;
             int goldCount = counts.GoldCount;
             int silverCount = counts.SilverCount;
@@ -55,7 +60,7 @@ namespace WindowsFormsApp1
 
         private void LoadSubscriptionCounts()
         {
-            var counts = FetchSubscriberCounts();
+            var counts = dashboardController.FetchSubscriberCounts();
 
             // Clear existing series data
             this.subscriptionChart.Series.Clear();
@@ -83,68 +88,9 @@ namespace WindowsFormsApp1
         }
 
 
-        public (int PlatinumCount, int GoldCount, int SilverCount) FetchSubscriberCounts()
-        {
-            List<string> subscriptionTypes = new List<string>();
-            int platinumCount = 0;
-            int goldCount = 0;
-            int silverCount = 0;
-
-            using (SqlConnection connection = dbConnection.GetConnection())
-            {
-                if (connection.State == System.Data.ConnectionState.Open)
-                {
-                    string query = "SELECT [Type] FROM subscriptions;";
-                    using (SqlCommand command = new SqlCommand(query, connection))
-                    {
-                        try
-                        {
-                            SqlDataReader reader = command.ExecuteReader();
-                            while (reader.Read())
-                            {
-                                string subscriptionType = reader["Type"].ToString();
-                                
-                                subscriptionTypes.Add(subscriptionType);
-                            }
-                            reader.Close();
-                        }
-                        catch (Exception ex)
-                        {
-                            Console.WriteLine("Error executing SQL query: " + ex.Message);
-                        }
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("Connection state is not open.");
-                }
-            }
-
-            Console.WriteLine(subscriptionTypes);
-            // Count the occurrences of each subscription type
-            foreach (string subscriptionType in subscriptionTypes)
-            {
-                Console.WriteLine(subscriptionType);
-                Console.WriteLine("-----------------");
-                if (subscriptionType.Contains("Platinum"))
-                {
-                    platinumCount = platinumCount+1;
-                }
-                else if (subscriptionType.Contains("Gold"))
-                {
-                    goldCount = goldCount+1;
-                }
-                else if (subscriptionType.Contains("Silver"))
-                {
-                    silverCount = silverCount +1;
-                }
-            }
-            return (platinumCount, goldCount, silverCount);
-        }
 
 
-
-
+        
 
 
         private void LoadData()
@@ -152,10 +98,10 @@ namespace WindowsFormsApp1
             try
             {
                 // Fetch and display gym statistics
-                int totalMembers = FetchTotalMembers();
-                int totalEqu = FetchTotalEqu();
-                int totalSession = FetchTotalSession();
-                int totalcheckin = FetchTotalCheckin();
+                int totalMembers = dashboardController.FetchTotalMembers();
+                int totalEqu = dashboardController.FetchTotalEqu();
+                int totalSession = dashboardController.FetchTotalSession();
+                int totalcheckin = dashboardController.FetchTotalCheckin();
 
                 label1.Text = totalMembers.ToString();
                 label2.Text =  totalEqu.ToString();
@@ -175,36 +121,7 @@ namespace WindowsFormsApp1
             }
         }
 
-        private int FetchTotalMembers()
-        {
-            using (SqlCommand command = new SqlCommand("SELECT COUNT(*) FROM Member", connection))
-            {
-                return (int)command.ExecuteScalar();
-            }
-        }
-        private int FetchTotalEqu()
-        {
-            using (SqlCommand command = new SqlCommand("SELECT COUNT(*) FROM Equipment", connection))
-            {
-                return (int)command.ExecuteScalar();
-            }
-        }
-
-        private int FetchTotalSession()
-        {
-            using (SqlCommand command = new SqlCommand("SELECT COUNT(*) FROM Sessions", connection))
-            {
-                return (int)command.ExecuteScalar();
-            }
-        }
-        private int FetchTotalCheckin()
-        {
-            using (SqlCommand command = new SqlCommand("SELECT COUNT(*) FROM checkins", connection))
-            {
-                return (int)command.ExecuteScalar();
-            }
-        }
-
+        
 
 
 
--- WindowsFormsApp1/views/EquipmentForms/EquipmentForm.Designer.cs
+++ WindowsFormsApp1/views/EquipmentForms/EquipmentForm.Designer.cs
@@ -354,7 +354,7 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 26;
-            this.dataGridView1.Size = new System.Drawing.Size(941, 322);
+            this.dataGridView1.Size = new System.Drawing.Size(966, 322);
             this.dataGridView1.TabIndex = 13;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
